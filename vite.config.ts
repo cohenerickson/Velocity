7c9d48378b0bea4f9e4d6289ff076294e48d3fce
Cohen Erickson <cohenerickson@gmail.com>
2023-02-09 13:33:10 -0600

    fix uv

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,10 @@ export default defineConfig({
           dest: "uv"
         },
         {
+          src: `${uvPath}/uv.client.js`,
+          dest: "uv"
+        },
+        {
           src: `public/uv/uv.config.js`,
           dest: "uv"
         }
