e3723d66047637f9a740e91af0059353a959f07e
Cohen Erickson <cohenerickson@gmail.com>
2023-02-22 15:12:31 -0600

    update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,10 +1,15 @@
+const apexHost = "radon.games";
+
 self.__uv$config = {
+  bare: [
+    `https://uv.${apexHost}/bare1/`,
+    `https://uv.${apexHost}/bare2/`,
+    `https://uv.${apexHost}/bare3/`
+  ],
   prefix: "/~/uv/",
-  bare: "/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
-  client: "/uv/uv.client.js",
   bundle: "/uv/uv.bundle.js",
   config: "/uv/uv.config.js",
   sw: "/uv/uv.sw.js"
