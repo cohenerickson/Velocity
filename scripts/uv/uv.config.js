04a21935591941d0afad2a97d06d7f31aded785c
Cohen Erickson <cohenerickson@gmail.com>
2023-03-23 14:44:17 -0500

    fix bare path on selfhost

--- scripts/uv/uv.config.js
+++ scripts/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-  bare: `${location.host}/bare/`,
+  bare: `/bare/`,
   prefix: "/~/uv/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
