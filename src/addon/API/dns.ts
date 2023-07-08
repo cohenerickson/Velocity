c88565f37f4c8d71ba59d8cbaa2b69e294e150f1
Cohen Erickson <cohenerickson@gmail.com>
2023-07-07 23:51:37 -0500

    Improve DNS api

--- src/addon/API/dns.ts
+++ src/addon/API/dns.ts
@@ -1,5 +1,7 @@
-// TODO: Implement virtual cache
-import { v4 } from "uuid";
+const Cache = new Map<string, DNSRecord>();
+const IPV6_REGEX =
+  /(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/;
+const IPV4_REGEX = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/;
 
 type Flag =
   | "allow_name_collisions"
@@ -13,18 +15,13 @@ type Flag =
   | "priority_medium"
   | "speculate";
 
-class DNSRecord {
+type DNSRecord = {
   addresses: string[];
-  canonicalName: string;
-  isTRR: boolean = true;
+  canonicalName?: string;
+  isTRR: boolean;
+};
 
-  constructor(addresses: string[], canonicalName: string = "") {
-    this.addresses = addresses;
-    this.canonicalName = canonicalName;
-  }
-}
-
-type GDNSAnswer = {
+type Answer = {
   name: string;
   type: number;
   TTL: number;
@@ -33,45 +30,66 @@ type GDNSAnswer = {
 
 export async function resolve(
   hostname: string,
-  flags: Flag[]
+  flags: Flag[] = []
 ): Promise<DNSRecord> {
-  let cname = "";
-  let url = `https://dns.google/resolve?name=${hostname}&type=A`;
+  const cached: DNSRecord = Cache.get(hostname) ?? {
+    addresses: [],
+    isTRR: true
+  };
 
-  if (flags.includes("bypass_cache")) {
-    url += `&cache=${v4()}`;
+  let updateCname = false;
+  let updateAddresses = false;
+
+  if (flags.includes("bypass_cache") || !cached.addresses.length) {
+    updateCname = true;
+    updateAddresses = true;
   }
 
-  if (flags.includes("canonical_name")) {
-    const response = await _$bareClient.fetch(url);
+  if (flags.includes("canonical_name") && !cached?.canonicalName) {
+    updateCname = true;
+  }
+
+  if (flags.includes("offline")) {
+    updateCname = false;
+    updateAddresses = false;
+  }
+
+  if (updateAddresses || updateCname) {
+    const response = await fetch(`https://1.1.1.1/dns-query?name=${hostname}`, {
+      headers: {
+        accept: "application/dns-json"
+      }
+    });
+
     const data = await response.json();
-    if (data.Anser) {
-      cname = (
-        data.Answer.filter((x: GDNSAnswer) => x.type === 5)[0] ?? { data: "" }
+
+    if (updateAddresses) {
+      cached.addresses =
+        data.Answer.filter((x: Answer) => x.type === 28 || x.type === 1) ?? [];
+    }
+
+    if (updateCname) {
+      cached.canonicalName = (
+        data.Answer.filter((x: Answer) => x.type === 5)[0] ?? {
+          data: undefined
+        }
       ).data;
     }
+
+    Cache.set(hostname, cached);
   }
 
-  const response = await _$bareClient.fetch(url);
-  const data = await response.json();
-  let addresses = [];
-
-  if (data.Answer) {
-    addresses =
-      data.Answer.filter((x: GDNSAnswer) => x.type === 28 || x.type === 1) ??
-      [];
-
-    if (flags.includes("disable_ipv4")) {
-      addresses = addresses.filter((x: GDNSAnswer) => x.type === 28);
-    }
-
-    if (flags.includes("disable_ipv6")) {
-      addresses = addresses.filter((x: GDNSAnswer) => x.type === 1);
-    }
+  if (flags.includes("disable_ipv4")) {
+    cached.addresses = cached.addresses.filter(IPV6_REGEX.test);
   }
 
-  return new DNSRecord(
-    addresses.map((x: GDNSAnswer) => x.data),
-    cname
-  );
+  if (flags.includes("disable_ipv6")) {
+    cached.addresses = cached.addresses.filter(IPV4_REGEX.test);
+  }
+
+  if (!flags.includes("canonical_name")) {
+    cached.canonicalName = undefined;
+  }
+
+  return cached;
 }
